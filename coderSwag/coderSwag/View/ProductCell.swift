c8c22665ec6de2fc4456bce83f4da841dc41b2fa
daramfon <Daramfon@buffalo.edu>
2019-07-11 19:57:39 -0400

    Collection views, creation of view controller

---
+++ coderSwag/coderSwag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coderSwag
+//
+//  Created by Daramfon Akpan on 7/11/19.
+//  Copyright © 2019 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage:UIImageView!
+    @IBOutlet weak var productTitle:UILabel!
+    @IBOutlet weak var productPrice:UILabel!
+    
+    
+    
+}
