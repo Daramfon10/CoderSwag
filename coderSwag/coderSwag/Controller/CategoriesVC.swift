77b10c131ebd9a69505bc0f01245d3619137e159
daramfon <Daramfon@buffalo.edu>
2019-07-10 21:33:06 -0400

    Project Creation & IBOutlets

---
+++ coderSwag/coderSwag/Controller/CategoriesVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  coderSwag
+//
+//  Created by Daramfon Akpan on 7/10/19.
+//  Copyright © 2019 Daramfon Akpan. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var catergoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
